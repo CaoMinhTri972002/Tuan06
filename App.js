55154b2ad96f24ac0176368a786d55cfdd5a8a0b
CaoMinhTri972002 <144448561+CaoMinhTri972002@users.noreply.github.com>
2024-10-03 18:49:52 +0700

    update

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'; 
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Screen1 from './screen/Screen1';
+import Screen2 from './screen/Screen2';
+import { Pressable, Image,Text } from 'react-native';
+import { View } from 'react-native-web';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function StackNavigator() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: '#1BA9FF' }, // Đặt màu nền cho header
+        headerTintColor: '#fff', // Màu chữ trên header
+        headerTitleStyle: { fontWeight: 'bold' },
+         // Kiểu chữ trên header
+      }}
+    >
+      <Stack.Screen name="Home" component={Screen1} options={{headerTitle:()=>(
+        <View style={{flexDirection:'row'}}>
+           <Pressable><Image source={require('./assets/ant-design_arrow-left-outlined.png')} 
+                   style={{height:24, width:24}} ></Image>
+                   </Pressable>
+                   <Text style={{marginLeft:120, color:'white'}}> Chat </Text>
+                   <Image source={require('./assets/bi_cart-check.png')} 
+                   style={{height:24, width:24,marginLeft:120}} ></Image>
+
+           </View>
+       )}} />
+    
+
+
+
+
+
+      
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={StackNavigator} options={{ tabBarBadge: 3 }} />
+        <Tab.Screen name="Settings" component={Screen2} options={{ tabBarBadge: 3 }} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
